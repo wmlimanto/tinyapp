4d0eb2b5c4247d1630cfc56fc1a5126bdb123e4c
Winston Limanto <wmlimanto@gmail.com>
2022-04-28 19:20:57 -0700

    edit param for shortURL path to display longURL

--- express_server.js
+++ express_server.js
@@ -31,6 +31,6 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
