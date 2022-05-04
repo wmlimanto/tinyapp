a3d21843e7e41dd15e5761191d2cb2274475eb7f
Winston Limanto <wmlimanto@gmail.com>
2022-05-05 00:55:15 +0200

    add endpoints to handle post requests for login and logout

--- express_server.js
+++ express_server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const { restart } = require("nodemon");
+const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
-function generateRandomString() {
+const generateRandomString = function() {
   return Math.random().toString(36).substring(2,8)
 };
 
@@ -27,30 +30,31 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, 
+    username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 // server renders urls_new template and user inputs new longURL to shorten
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { urls: urlDatabase,
+    username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 // server generates a shortURL and adds it to database
 app.post("/urls", (req, res) => {
-  res.status(200);
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL], 
+    username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -73,4 +77,17 @@ app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect("/urls");
+});
+
+// endpoint to handle post to login
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect('/urls');
+});
+
+// endpoint to handle post to logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
 });
